a5ad07ff01827ee2207798d308609f2b4fe69fae
Jiongan Mu <max1078735979@gmail.com>
2025-03-12 00:11:01 -0400

    Fix PostgreSQL data loading script path quoting

--- postgres/load_data.sh
+++ postgres/load_data.sh
@@ -1,2 +1,2 @@
 
-$PWD/msvbase_install/postgres/bin/psql -d vectordb -v data_path="'$PWD/processed_data/base_vectors/sift_base_normal.tsv'" -f $PWD/postgres/load_data.sql
+$PWD/msvbase_install/postgres/bin/psql -d vectordb -v data_path="$PWD/processed_data/base_vectors/sift_base_normal.tsv" -f $PWD/postgres/load_data.sql
