d8d2bca522f31460ab198dfd07f51091009cdfbc
Jiongan Mu <max1078735979@gmail.com>
2025-03-13 10:40:01 -0400

    Update run_all_combinations.sh to use relative path for run.sh execution, ensuring compatibility across different environments.

--- run_all_combinations.sh
+++ run_all_combinations.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 chmod +x run.sh
 
-run.sh --distribution normal --selectivity 1
-run.sh --distribution normal --selectivity 10
-run.sh --distribution normal --selectivity 50
-run.sh --distribution normal --selectivity 90
-run.sh --distribution normal --selectivity 99
-run.sh --distribution normal --selectivity 100
+./run.sh --distribution normal --selectivity 1
+./run.sh --distribution normal --selectivity 10
+./run.sh --distribution normal --selectivity 50
+./run.sh --distribution normal --selectivity 90
+./run.sh --distribution normal --selectivity 99
+./run.sh --distribution normal --selectivity 100
 
 
-run.sh --distribution zipfine --selectivity 1
-run.sh --distribution zipfine --selectivity 10
-run.sh --distribution zipfine --selectivity 50
-run.sh --distribution zipfine --selectivity 90
-run.sh --distribution zipfine --selectivity 99
-run.sh --distribution zipfine --selectivity 100
+./run.sh --distribution zipfine --selectivity 1
+./run.sh --distribution zipfine --selectivity 10
+./run.sh --distribution zipfine --selectivity 50
+./run.sh --distribution zipfine --selectivity 90
+./run.sh --distribution zipfine --selectivity 99
+./run.sh --distribution zipfine --selectivity 100
 
-run.sh --distribution uniform --selectivity 1
-run.sh --distribution uniform --selectivity 10
-run.sh --distribution uniform --selectivity 50
-run.sh --distribution uniform --selectivity 90
-run.sh --distribution uniform --selectivity 99
-run.sh --distribution uniform --selectivity 100
+./run.sh --distribution uniform --selectivity 1
+./run.sh --distribution uniform --selectivity 10
+./run.sh --distribution uniform --selectivity 50
+./run.sh --distribution uniform --selectivity 90
+./run.sh --distribution uniform --selectivity 99
+./run.sh --distribution uniform --selectivity 100
 
 
-run.sh --distribution log_normal --selectivity 1
-run.sh --distribution log_normal --selectivity 10
-run.sh --distribution log_normal --selectivity 50
-run.sh --distribution log_normal --selectivity 90
-run.sh --distribution log_normal --selectivity 99
-run.sh --distribution log_normal --selectivity 100
+./run.sh --distribution log_normal --selectivity 1
+./run.sh --distribution log_normal --selectivity 10
+./run.sh --distribution log_normal --selectivity 50
+./run.sh --distribution log_normal --selectivity 90
+./run.sh --distribution log_normal --selectivity 99
+./run.sh --distribution log_normal --selectivity 100
